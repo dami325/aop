4c6f5d07964fbd7e874449ff627605db401a2851
Judalm Park (dami) <wnekfa1004@naver.com>
2023-06-01 22:56:46 +0900

    스프링 AOP 구현6 - 어드바이스 종류

--- src/test/java/hello/aop/AopTest.java
+++ src/test/java/hello/aop/AopTest.java
@@ -5,6 +5,7 @@ import hello.aop.order.OrderService;
 import hello.aop.order.aop.AspectV3;
 import hello.aop.order.aop.AspectV4Pointcut;
 import hello.aop.order.aop.AspectV5Order;
+import hello.aop.order.aop.AspectV6Advice;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.aop.support.AopUtils;
@@ -21,7 +22,8 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 //@Import(AspectV2.class)
 //@Import(AspectV3.class)
 //@Import(AspectV4Pointcut.class)
-@Import({AspectV5Order.LogAspect.class, AspectV5Order.TxAspect.class})
+//@Import({AspectV5Order.LogAspect.class, AspectV5Order.TxAspect.class})
+@Import(AspectV6Advice.class)
 public class AopTest {
 
     @Autowired
